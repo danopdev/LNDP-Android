97d0efb69e347d6ab3377e4482acbdc90cc75a10
dan <doprea@gmail.com>
2021-01-31 15:53:50 +0100

    Use binding

--- app/src/main/java/com/dan/lndpandroid/MainActivity.kt
+++ app/src/main/java/com/dan/lndpandroid/MainActivity.kt
@@ -2,10 +2,14 @@ package com.dan.lndpandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dan.lndpandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
     }
 }
