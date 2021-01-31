9041a7d60f3658036b0b09c0f4c7ed318fe7e10a
dan <doprea@gmail.com>
2021-01-31 15:42:41 +0100

    Add settings class

---
+++ app/src/main/java/com/dan/lndpandroid/Settings.kt
@@ -0,0 +1,58 @@
+package com.dan.lndpandroid
+
+import android.app.Activity
+import android.content.Context
+import kotlin.reflect.*
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.declaredMemberProperties
+
+/**
+Settings: all public var fields will be save / restaured
+ */
+class Settings( private val activity: Activity) {
+
+    var serverUri: String = ""
+
+    init {
+        loadProperties()
+    }
+
+    private fun forEachSettingProperty( listener: (KMutableProperty<*>)->Unit ) {
+        for( member in this::class.declaredMemberProperties ) {
+            if (member.visibility == KVisibility.PUBLIC && member is KMutableProperty<*>) {
+                listener.invoke(member)
+            }
+        }
+    }
+
+    private fun loadProperties() {
+        val preferences = activity.getPreferences(Context.MODE_PRIVATE)
+
+        forEachSettingProperty { property ->
+            when( property.returnType ) {
+                Boolean::class.createType() -> property.setter.call( this, preferences.getBoolean( property.name, property.getter.call(this) as Boolean ) )
+                Int::class.createType() -> property.setter.call( this, preferences.getInt( property.name, property.getter.call(this) as Int ) )
+                Long::class.createType() -> property.setter.call( this, preferences.getLong( property.name, property.getter.call(this) as Long ) )
+                Float::class.createType() -> property.setter.call( this, preferences.getFloat( property.name, property.getter.call(this) as Float ) )
+                String::class.createType() -> property.setter.call( this, preferences.getString( property.name, property.getter.call(this) as String ) )
+            }
+        }
+    }
+
+    fun saveProperties() {
+        val preferences = activity.getPreferences(Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+
+        forEachSettingProperty { property ->
+            when( property.returnType ) {
+                Boolean::class.createType() -> editor.putBoolean( property.name, property.getter.call(this) as Boolean )
+                Int::class.createType() -> editor.putInt( property.name, property.getter.call(this) as Int )
+                Long::class.createType() -> editor.putLong( property.name, property.getter.call(this) as Long )
+                Float::class.createType() -> editor.putFloat( property.name, property.getter.call(this) as Float )
+                String::class.createType() -> editor.putString( property.name, property.getter.call(this) as String )
+            }
+        }
+
+        editor.commit()
+    }
+}
