e499948568fb8375f77560cd1a7ee59632936398
dan <doprea@gmail.com>
2021-02-01 20:44:08 +0100

    Add ktor server

--- app/src/main/java/com/dan/lndpandroid/MainActivity.kt
+++ app/src/main/java/com/dan/lndpandroid/MainActivity.kt
@@ -3,6 +3,13 @@ package com.dan.lndpandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dan.lndpandroid.databinding.ActivityMainBinding
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.http.ContentType
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.embeddedServer
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +18,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
+        startServer()
+    }
+
+    private fun startServer() {
+        val server = embeddedServer(CIO, port = 8080) {
+            routing {
+                get("/") {
+                    call.respondText("Hello World!", ContentType.Text.Plain)
+                }
+                get("/demo") {
+                    call.respondText("HELLO WORLD!")
+                }
+            }
+        }
+        server.start()
     }
 }
