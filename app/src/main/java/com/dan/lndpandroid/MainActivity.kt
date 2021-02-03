874da80d2c91f19e56b9d89c99c56083669be5ac
dan <doprea@gmail.com>
2021-02-03 07:49:04 +0100

    Upgrade to ktor 1.5.1

--- app/src/main/java/com/dan/lndpandroid/MainActivity.kt
+++ app/src/main/java/com/dan/lndpandroid/MainActivity.kt
@@ -8,8 +8,8 @@ import io.ktor.response.respondText
 import io.ktor.http.ContentType
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startServer() {
-        val server = embeddedServer(CIO, port = 8080) {
+        val server = embeddedServer(Netty, port = 8080) {
             routing {
                 get("/") {
                     call.respondText("Hello World!", ContentType.Text.Plain)
