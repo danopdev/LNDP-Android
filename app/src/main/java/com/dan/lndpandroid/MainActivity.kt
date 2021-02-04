cdb30bdd1b5934147df2b833b34481ebd0a31175
dan <doprea@gmail.com>
2021-02-04 20:39:25 +0100

    Handle permissions

--- app/src/main/java/com/dan/lndpandroid/MainActivity.kt
+++ app/src/main/java/com/dan/lndpandroid/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.dan.lndpandroid
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dan.lndpandroid.databinding.ActivityMainBinding
 import io.ktor.application.call
 import io.ktor.response.respondText
@@ -10,15 +17,101 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val PERMISSIONS = arrayOf(
+            Manifest.permission.INTERNET
+        )
+
+        const val REQUEST_PERMISSIONS = 1
+        const val INTENT_SELECT_FOLDER = 2
+    }
 
     private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!askPermissions()) onPermissionsAllowed()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSIONS -> handleRequestPermissions(grantResults)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        if (INTENT_SELECT_FOLDER == requestCode && RESULT_OK == resultCode && null != intent) {
+            /*
+            val data = intent.data
+            if (data is Uri) {
+                mSaveFolder = DocumentFile.fromTreeUri(applicationContext, data)
+                settings.saveUri = data.toString()
+                settings.saveProperties()
+
+                if (mFirstCall) onValidSaveFolder()
+            }
+            */
+        }
+    }
+
+    private fun onPermissionsAllowed() {
         setContentView(mBinding.root)
         startServer()
+    }
+
+    private fun askPermissions(): Boolean {
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSIONS)
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private fun handleRequestPermissions(grantResults: IntArray) {
+        if (grantResults.size < PERMISSIONS.size)
+            return
+
+        var allowedAll = grantResults.size >= PERMISSIONS.size
+
+        if (grantResults.size >= PERMISSIONS.size) {
+            for ( result in grantResults ) {
+                if (result != PackageManager.PERMISSION_GRANTED ) {
+                    allowedAll = false
+                    break
+                }
+            }
+        }
+
+        if( allowedAll ) {
+            onPermissionsAllowed()
+        } else {
+            fatalError("Permissions are mandatory !")
+        }
+    }
+
+    private fun exitApp() {
+        setResult(0)
+        finish()
+        exitProcess(0)
+    }
+
+    private fun fatalError(msg: String) {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.app_name))
+            .setMessage(msg)
+            .setIcon(android.R.drawable.stat_notify_error)
+            .setCancelable(false)
+            .setPositiveButton("OK") { _, _ -> exitApp() }
+            .show()
     }
 
     private fun startServer() {
