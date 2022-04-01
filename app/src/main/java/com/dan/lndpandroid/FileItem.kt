3706ebad44ad2b3b444a39cef2ad5ad78af15fff
Dan Oprea <dan.oprea@masternaut.com>
2022-04-01 15:49:21 +0200

    Format file size: B / KB / MB / GB

--- app/src/main/java/com/dan/lndpandroid/FileItem.kt
+++ app/src/main/java/com/dan/lndpandroid/FileItem.kt
@@ -3,6 +3,24 @@ package com.dan.lndpandroid
 import android.graphics.Bitmap
 
 class FileItem(val file: UriFile, var thumbnail: Bitmap? = null ) {
-    val details = "${file.date}  |  ${file.length} byte(s)"
+    companion object {
+        private const val KB = 1024L
+        private const val MB = KB * KB
+        private const val GB = KB * MB
+
+        fun formatSize(sizeInBytes: Long): String {
+            return when {
+                sizeInBytes < KB -> "$sizeInBytes B"
+                sizeInBytes < (10L * KB) -> "${String.format("%.2f", sizeInBytes / KB.toDouble())} KB"
+                sizeInBytes < MB -> "${sizeInBytes / KB} KB"
+                sizeInBytes < (10L * MB) -> "${String.format("%.2f", sizeInBytes / MB.toDouble())} MB"
+                sizeInBytes < GB -> "${sizeInBytes / MB} MB"
+                sizeInBytes < (10L * GB) -> "${String.format("%.2f", sizeInBytes / GB.toDouble())} GB"
+                else -> "${sizeInBytes / GB} GB"
+            }
+        }
+    }
+
+    val details = "${file.date}  |  ${formatSize(file.length)}"
     var isSelected = false
 }
